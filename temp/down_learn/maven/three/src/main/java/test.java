0e28e935ed623dcaea884079743498194e8b98be
jonekaka <973579614@qq.com>
2020-09-05 16:52:01 +0800

    maven基础

---
+++ temp/down_learn/maven/three/src/main/java/test.java
@@ -0,0 +1,14 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class test extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/a.jsp").forward(req,resp);
+    }
+}
